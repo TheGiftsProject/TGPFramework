dec3aa8b950b4389b2363c79c7d2adb8fcaddffc
Gartner <gartner@giftsproject.com>
2011-02-10 20:37:14 +0200

    Refactored state.js

--- fsm/state.js
+++ fsm/state.js
@@ -9,7 +9,14 @@ ObjectRepository.Require('TGP.FSM.State', function() {
         this.unload      = unload;
         this.status      = TGP.FSM.State.STATUS.PRE_INIT;
         this.OnCancel    = new TGP.Utils.Observer();
+        this.OnInit      = new TGP.Utils.Observer();
+        this.OnLoad      = new TGP.Utils.Observer();
+        this.OnUnload    = new TGP.Utils.Observer();
+
         this.OnCancel.AddListener(this.ResetStatus, this);
+        this.OnInit.AddListener(this.OnInitDone, this);
+        this.OnLoad.AddListener(this.OnLoadDone, this);
+        this.OnUnload.AddListener(this.OnUnloadDone, this);
     };
 
     TGP.FSM.State.STATUS = {
@@ -24,76 +31,76 @@ ObjectRepository.Require('TGP.FSM.State', function() {
     TGP.FSM.State.prototype.ResetStatus = function() {
         switch (this.status) {
             case TGP.FSM.State.STATUS.UNLOADING:
-                this.status = TGP.FSM.State.STATUS.ACTIVE;
+                this.ChangeStatus(TGP.FSM.State.STATUS.ACTIVE);
                 break;
             case TGP.FSM.State.STATUS.LOADING:
-                this.status = TGP.FSM.State.STATUS.INITIALIZED;
+                this.ChangeStatus(TGP.FSM.State.STATUS.INITIALIZED);
                 break;
             case TGP.FSM.State.STATUS.INITIALIZING:
-                this.status = TGP.FSM.State.STATUS.PRE_INIT;
+                this.ChangeStatus(TGP.FSM.State.STATUS.PRE_INIT);
                 break;
         }
     };
 
-    TGP.FSM.State.prototype.loadState = function(postLoadCallback) {
-        var thisState = this;
-
-        function DoneInitCallback(errorMessage) {
-            if (errorMessage) {
-                thisState.OnCancel.Trigger(errorMessage);
-                return;
-            }
-
-            thisState.status = TGP.FSM.State.STATUS.INITIALIZED;
-            TGP.Utils.Logger.Info('Initialized: ' + thisState.stateName);
-
-            if (thisState.load && thisState.status === TGP.FSM.State.STATUS.INITIALIZED) {
-                thisState.status = TGP.FSM.State.STATUS.LOADING;
-                thisState.load(DoneLoadCallback);
-            } else {
-                DoneLoadCallback();
-            }
-        }
-
-        function DoneLoadCallback(errorMessage) {
-            if (errorMessage) {
-                thisState.OnCancel.Trigger(errorMessage);
-                return;
-            }
-
-            thisState.status = TGP.FSM.State.STATUS.ACTIVE;
-            TGP.Utils.Logger.Info('Loaded: ' + thisState.stateName);
-            if (postLoadCallback) { postLoadCallback(); }
-        }
-
-        if (this.init && this.status === TGP.FSM.State.STATUS.PRE_INIT) {
-            this.status = TGP.FSM.State.STATUS.INITIALIZING;
-            this.init(DoneInitCallback);
-        } else {
-            DoneInitCallback();
+    TGP.FSM.State.prototype.ChangeStatus = function(newStatus) {
+        if (this.status !== newStatus) {
+            this.status = newStatus;
         }
     };
 
-    TGP.FSM.State.prototype.unloadState = function(postUnloadCallback) {
-        var thisState = this;
-
-        function DoneUnloadCallback(errorMessage) {
-            if (errorMessage) {
-                thisState.OnCancel.Trigger(errorMessage);
-                return;
-            }
-
-            thisState.status = TGP.FSM.State.STATUS.INITIALIZED;
-            TGP.Utils.Logger.Info('Unloaded: ' + thisState.stateName);
-            if (postUnloadCallback) { postUnloadCallback(); }
-        }
-
-        if (this.unload && this.status === TGP.FSM.State.STATUS.ACTIVE) {
-            this.status = TGP.FSM.State.STATUS.UNLOADING;
-            this.unload(DoneUnloadCallback);
+    TGP.FSM.State.prototype.loadState = function() {
+        if (this.init && this.status === TGP.FSM.State.STATUS.PRE_INIT) {
+            this.ChangeStatus(TGP.FSM.State.STATUS.INITIALIZING);
+            this.init(this.OnInit.GetBoundTrigger());
         } else {
-            DoneUnloadCallback();
+            this.OnInit.Trigger();
         }
+    };
+
+    TGP.FSM.State.prototype.OnInitDone = function(errorMessage) {
+        if (errorMessage) {
+            this.OnCancel.Trigger(errorMessage);
+            return;
+        }
+
+        this.ChangeStatus(TGP.FSM.State.STATUS.INITIALIZED);
+        TGP.Utils.Logger.Info('Initialized: ' + this.stateName);
+
+        if (this.load && this.status === TGP.FSM.State.STATUS.INITIALIZED) {
+            this.ChangeStatus(TGP.FSM.State.STATUS.LOADING);
+            this.load(this.OnLoad.GetBoundTrigger());
+        } else {
+            this.OnLoad.Trigger();
+        }
+    };
+
+    TGP.FSM.State.prototype.OnLoadDone = function(errorMessage) {
+        if (errorMessage) {
+            this.OnCancel.Trigger(errorMessage);
+            return;
+        }
+
+        this.ChangeStatus(TGP.FSM.State.STATUS.ACTIVE);
+        TGP.Utils.Logger.Info('Loaded: ' + this.stateName);
+    };
+
+    TGP.FSM.State.prototype.unloadState = function() {
+        if (this.unload && this.status === TGP.FSM.State.STATUS.ACTIVE) {
+            this.ChangeStatus(TGP.FSM.State.STATUS.UNLOADING);
+            this.unload(this.OnUnload.GetBoundTrigger());
+        } else {
+            this.OnUnload.Trigger();
+        }
+    };
+
+    TGP.FSM.State.prototype.OnUnloadDone = function(errorMessage) {
+        if (errorMessage) {
+            this.OnCancel.Trigger(errorMessage);
+            return;
+        }
+
+        this.ChangeStatus(TGP.FSM.State.STATUS.INITIALIZED);
+        TGP.Utils.Logger.Info('Unloaded: ' + this.stateName);
     };
 
 }, true);
