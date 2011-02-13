b45ca620421bdcb9669299842fd418d512c721ed
Gartner <gartner@giftsproject.com>
2011-02-13 19:57:57 +0200

    More work on FSM

--- fsm/state.js
+++ fsm/state.js
@@ -2,43 +2,50 @@ ObjectRepository.Require('TGP.FSM.State', function() {
 
     TGP.Namespace('FSM');
 
-    TGP.FSM.State = function(stateName, init, load, unload) {
-        this.stateName   = stateName;
-        this.init        = init;
-        this.load        = load;
-        this.unload      = unload;
+    TGP.FSM.State = function() {
         this.status      = TGP.FSM.State.STATUS.PRE_INIT;
+
+        this.init = this.load = this.unload = null;
+        this.loadAfterInit = false;
+
         this.OnCancel    = new TGP.Utils.Observer();
         this.OnInit      = new TGP.Utils.Observer();
         this.OnLoad      = new TGP.Utils.Observer();
         this.OnUnload    = new TGP.Utils.Observer();
 
         this.OnCancel.AddListener(this.ResetStatus, this);
-        this.OnInit.AddListener(this.OnInitDone, this);
-        this.OnLoad.AddListener(this.OnLoadDone, this);
-        this.OnUnload.AddListener(this.OnUnloadDone, this);
+        this.OnCancel.AddListener(this.LogCancel, this);
+        this.OnInit.AddListener(this.InitDone, this);
+        this.OnLoad.AddListener(this.LoadDone, this);
+        this.OnUnload.AddListener(this.UnloadDone, this);
     };
 
     TGP.FSM.State.STATUS = {
         PRE_INIT:     0,
         INITIALIZING: 1,
-        INITIALIZED:  2,
+        UNLOADED:     2,
         LOADING:      3,
-        ACTIVE:       4,
+        LOADED:       4,
         UNLOADING:    5
     };
 
     TGP.FSM.State.prototype.ResetStatus = function() {
         switch (this.status) {
             case TGP.FSM.State.STATUS.UNLOADING:
-                this.ChangeStatus(TGP.FSM.State.STATUS.ACTIVE);
+                this.ChangeStatus(TGP.FSM.State.STATUS.LOADED);
                 break;
             case TGP.FSM.State.STATUS.LOADING:
-                this.ChangeStatus(TGP.FSM.State.STATUS.INITIALIZED);
+                this.ChangeStatus(TGP.FSM.State.STATUS.UNLOADED);
                 break;
             case TGP.FSM.State.STATUS.INITIALIZING:
                 this.ChangeStatus(TGP.FSM.State.STATUS.PRE_INIT);
                 break;
+        }
+    };
+
+    TGP.FSM.State.prototype.LogCancel = function(errorMessage) {
+        if (errorMessage) {
+            TGP.Utils.Logger.Warn(errorMessage);
         }
     };
 
@@ -48,58 +55,62 @@ ObjectRepository.Require('TGP.FSM.State', function() {
         }
     };
 
-    TGP.FSM.State.prototype.loadState = function() {
-        if (this.init && this.status === TGP.FSM.State.STATUS.PRE_INIT) {
+    TGP.FSM.State.prototype.InitState = function() {
+        if (this.status === TGP.FSM.State.STATUS.PRE_INIT) {
             this.ChangeStatus(TGP.FSM.State.STATUS.INITIALIZING);
-            this.init(this.OnInit.GetBoundTrigger());
-        } else {
-            this.OnInit.Trigger();
+
+            if (this.init) {
+                this.init({ finish: this.OnInit.GetBoundTrigger(), cancel: this.OnCancel.GetBoundTrigger() });
+            } else {
+                this.InitDone();
+            }
         }
     };
 
-    TGP.FSM.State.prototype.OnInitDone = function(errorMessage) {
-        if (errorMessage) {
-            this.OnCancel.Trigger(errorMessage);
-            return;
-        }
+    TGP.FSM.State.prototype.LoadState = function() {
+        if (this.status === TGP.FSM.State.STATUS.PRE_INIT) {
+            this.loadAfterInit = true;
+            this.InitState();
+        } else if (this.status === TGP.FSM.State.STATUS.UNLOADED) {
+            this.ChangeStatus(TGP.FSM.State.STATUS.LOADING);
 
-        this.ChangeStatus(TGP.FSM.State.STATUS.INITIALIZED);
+            if (this.load) {
+                this.load({ finish: this.OnLoad.GetBoundTrigger(), cancel: this.OnCancel.GetBoundTrigger() });
+            } else {
+                this.OnLoad.Trigger();
+            }
+        }
+    };
+
+    TGP.FSM.State.prototype.UnloadState = function() {
+        if (this.status === TGP.FSM.State.STATUS.LOADED) {
+            this.ChangeStatus(TGP.FSM.State.STATUS.UNLOADING);
+
+            if (this.unload) {
+                this.unload({ finish: this.OnUnload.GetBoundTrigger(), cancel: this.OnCancel.GetBoundTrigger() });
+            } else {
+                this.OnUnload.Trigger();
+            }
+        }
+    };
+
+    TGP.FSM.State.prototype.InitDone = function() {
+        this.ChangeStatus(TGP.FSM.State.STATUS.UNLOADED);
         TGP.Utils.Logger.Info('Initialized: ' + this.stateName);
 
-        if (this.load && this.status === TGP.FSM.State.STATUS.INITIALIZED) {
-            this.ChangeStatus(TGP.FSM.State.STATUS.LOADING);
-            this.load(this.OnLoad.GetBoundTrigger());
-        } else {
-            this.OnLoad.Trigger();
+        if (this.loadAfterInit === true) {
+            this.loadAfterInit = false;
+            this.LoadState();
         }
     };
 
-    TGP.FSM.State.prototype.OnLoadDone = function(errorMessage) {
-        if (errorMessage) {
-            this.OnCancel.Trigger(errorMessage);
-            return;
-        }
-
-        this.ChangeStatus(TGP.FSM.State.STATUS.ACTIVE);
+    TGP.FSM.State.prototype.LoadDone = function() {
+        this.ChangeStatus(TGP.FSM.State.STATUS.LOADED);
         TGP.Utils.Logger.Info('Loaded: ' + this.stateName);
     };
 
-    TGP.FSM.State.prototype.unloadState = function() {
-        if (this.unload && this.status === TGP.FSM.State.STATUS.ACTIVE) {
-            this.ChangeStatus(TGP.FSM.State.STATUS.UNLOADING);
-            this.unload(this.OnUnload.GetBoundTrigger());
-        } else {
-            this.OnUnload.Trigger();
-        }
-    };
-
-    TGP.FSM.State.prototype.OnUnloadDone = function(errorMessage) {
-        if (errorMessage) {
-            this.OnCancel.Trigger(errorMessage);
-            return;
-        }
-
-        this.ChangeStatus(TGP.FSM.State.STATUS.INITIALIZED);
+    TGP.FSM.State.prototype.UnloadDone = function() {
+        this.ChangeStatus(TGP.FSM.State.STATUS.UNLOADED);
         TGP.Utils.Logger.Info('Unloaded: ' + this.stateName);
     };
 
--- utils/observer.js
+++ utils/observer.js
@@ -30,13 +30,27 @@ ObjectRepository.Require('TGP.Utils.Observer', function() {
         return this;
     };
 
+    TGP.Utils.Observer.prototype.DetachCaller = function(caller) {
+        for (var i = 0; i < this.handlers.length; ++i)
+        {
+            if (this.handlers[i].caller === caller)
+            {
+                this.handlers.splice(i, 1);
+                i--;
+            }
+        }
+
+        return this;
+    };
+
     TGP.Utils.Observer.prototype.Trigger = function() {
         if (this.delayed) {
             this._TriggerDelayed.apply(this, arguments);
         } else {
-            for (var i = 0; i < this.handlers.length; ++i) {
-                if (this.handlers[i]) {
-                    this.handlers[i].listener.apply(this.handlers[i].caller, arguments);
+            var handlersCopy = TGP.Core.DeepCopy(this.handlers);
+            for (var i = 0; i < handlersCopy.length; ++i) {
+                if (handlersCopy[i]) {
+                    handlersCopy[i].listener.apply(handlersCopy[i].caller, arguments);
                 }
             }
         }
@@ -49,7 +63,7 @@ ObjectRepository.Require('TGP.Utils.Observer', function() {
     // This mess allows us to run the UI thread between each handler. This works well if you have many handlers that do small tasks. If you have several large handlers, they should be broken up with setTimeout themselves.
     TGP.Utils.Observer.prototype._TriggerDelayed = function() {
         var i = 0;
-        var handlers = this.handlers;
+        var handlers = TGP.Core.DeepCopy(this.handlers);
         var args = arguments;
         var worker;
         worker = function() {
